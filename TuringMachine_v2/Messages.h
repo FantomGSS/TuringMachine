c2d23f5d9b1c339c266801128244c458d44ff472
FantomGSS <gogo.stanchev@gmail.com>
2020-12-18 13:40:56 +0200

    Classes - Head, FileManager and Messages

---
+++ TuringMachine_v2/Messages.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+const std::string INVALID_PATH = "You have entered an invalid path! Forbidden characters are: / < | >";
+const std::string ENTER_FULL_PATH = "Enter the full path to the file: ";
+const std::string NOT_ENTERED_PATH = "You have not entered a path to the file!";
+const std::string FILE_NOT_EXIST = "The file does not exist!";
+const std::string FILE_EXISTS_SAME_NAME = "There is already a file with that name! Please enter a different path in the file system.";
+
+const std::string PROCESSED_SUCCESSFULLY = "The tape was processed successfully!";
+const std::string NOT_PROCESSED_SUCCESSFULLY = "The tape was not processed successfully!";
