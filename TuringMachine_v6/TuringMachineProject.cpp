5f832c035811942cd6d21c3e820ace709fffe6c4
FantomGSS <gogo.stanchev@gmail.com>
2020-12-29 13:51:20 +0200

    Added inputs, detected and fixed bugs in TM

---
+++ TuringMachine_v6/TuringMachineProject.cpp
@@ -0,0 +1,12 @@
+#include "TuringMachine.h"
+
+int main()
+{
+	TuringMachine one = TuringMachine();
+	TuringMachine two = TuringMachine();
+	//TuringMachine three = TuringMachine();
+
+	//one.run();
+	one.compose(two);
+	//one.continueWith(two, three);
+}
